7bdddb3dc2bb3dd4791140e0fff74368030fac68
Abhishek Choudhury <115447569+abhishekc-bhs@users.noreply.github.com>
2023-09-01 17:25:14 +0530

    add auth and core models for files, dir and proj

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Project, Directory, File
 
-# Register your models here.
+class DirectoryInline(admin.TabularInline):
+    model = Directory
+
+class FileInline(admin.TabularInline):
+    model = File
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at')
+    inlines = [DirectoryInline]
+
+class DirectoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'parent_dir', 'created_at')
+    inlines = [FileInline]
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'directory', 'created_at', 'updated_at')
+    search_fields = ('name', 'directory__name', 'directory__project__name')
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Directory, DirectoryAdmin)
+admin.site.register(File, FileAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from authentication.models import CustomUser as User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Directory(models.Model):
+    name = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    parent_dir = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
